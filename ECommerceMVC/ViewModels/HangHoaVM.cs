f4f664f429ff602aa04beea1109066726a83cb2e
ngokhaihuynh <ngokhaihuynh1405@gmail.com>
2024-12-27 20:42:15 +0700

    Upload

---
+++ ECommerceMVC/ViewModels/HangHoaVM.cs
@@ -0,0 +1,25 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+    public class HangHoaVM
+    {
+        public int MaHh { get; set; }
+        public String TenHH { get; set; }
+        public String Hinh { get; set; }
+        public double DonGia { get; set; }
+        public String MoTaNgan { get; set; }
+        public String TenLoai { get; set; }
+    }
+
+    public class ChiTietHangHoaVM
+    {
+        public int MaHh { get; set; }
+        public String TenHH { get; set; }
+        public String Hinh { get; set; }
+        public double DonGia { get; set; }
+        public String MoTaNgan { get; set; }
+        public String TenLoai { get; set; }
+        public String ChiTiet { get; set; }
+        public int DiemDanhGia { get; set; }
+        public int SoLuongTon { get; set; }
+    }
+}
---
+++ ECommerceMVC/ViewModels/MenuLoaiVM.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+    public class MenuLoaiVM
+    {
+        public int MaLoai { get; set; }
+        public String TenLoai { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
